92e05c0ce7ce0cd3c603835e0cc7ad8fe65ddb53
gsai <1481958050@qq.com>
2015-10-24 23:20:11 +0800

    https://github.com/gsai/leetcode.git

--- PRI64/generateParenthesis.cpp
+++ PRI64/generateParenthesis.cpp
@@ -9,6 +9,32 @@ class Solution
 public:
 	vector<string> generateParenthesis(int n)
 	{
+		vector<string> vecStr;
+		string s;
+		generateUtil(0, 0, n, vecStr, s);
 
+		return vecStr;
+	}
+	
+	void generateUtil(int left, int right, int n, vector<string> &alls, string str)
+	{
+		if (right == n)
+		{
+			alls.push_back(str);
+			return;
+		}
+		
+		if (left < n)
+		{
+			str += "(";
+			generateUtil(left + 1, right, n, alls, str);
+			str.pop_back();
+		}
+		if (left > right)
+		{
+			str += ")";
+			generateUtil(left, right + 1, n, alls, str);
+		}
 	}
 };
+
