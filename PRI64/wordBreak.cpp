6544a511643fbb417340b7b20e8a0c32d8831156
gsai <1481958050@qq.com>
2015-10-29 13:47:36 +0800

    DP 解决，比之前的快

--- PRI64/wordBreak.cpp
+++ PRI64/wordBreak.cpp
@@ -1,43 +1,30 @@
 #include <iostream>
-#include <unordered_set>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
-class Solution{
+class Solution
+{
 public:
-	vector< vector<string> > lists;
-	void split(string s, unordered_set<string> &dict, vector<string> vec){
-		if (s.length() == 0){
-			if (vec.size() != 0){
-				lists.push_back(vec);
-			}
-			return;
-		}
-		else
-		{
-			for (auto it = dict.begin(); it != dict.end(); it++){
-				string subStr(s.substr(0, (*it).length()));
-				if (subStr.compare((*it)) == 0){
-					vec.push_back(subStr);
-					split(s.substr(subStr.length(), s.length() - subStr.length()), dict, vec);
-					vec.pop_back();
-				}
-			}
-		}
-	}
-	bool wordBreak(string s, unordered_set<string>& wordDict) {
-		vector<string> vec;
-		split(s, wordDict, vec);
-		if (lists.size() != 0)
-		{
-			return true;
-		}
-		else
-		{
+
+	bool wordBreak(string s, unordered_set<string> &wordDict)
+	{
+		if (s.empty() || wordDict.empty())
 			return false;
+		string::size_type length = s.size() + 1;
+		vector<bool> estimate(length, false);
+		estimate[0] = true;
+		for (int i = 1; i < length; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				if (estimate[j] && wordDict.find(s.substr(j, i - j)) != wordDict.end())
+					estimate[i] = true;
+			}
 		}
+		return estimate.back();
 	}
 };
 
