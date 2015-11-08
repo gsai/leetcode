ebfade3e0c08d5e739ece78a23810cadbda3a0d2
gsai <1481958050@qq.com>
2015-11-08 16:03:32 +0800

    concise solution from the web

--- PRI64/MedianFinder.cpp
+++ PRI64/MedianFinder.cpp
@@ -7,6 +7,20 @@ private:
 public:
 	void addNum(int num)
 	{
+		small.push(num);
+		large.push(-small.top());
+		small.pop();
+		if (small.size() < large.size())
+		{
+			small.push(-large.top());
+			large.pop();
+		}
+	}
 
+	double findMedian()
+	{
+		return small.size()>large.size()
+			? small.top()
+			: (small.top() - large.top()) / 2.0;
 	}
 };
